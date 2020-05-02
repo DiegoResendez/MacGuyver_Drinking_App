e2dc24bad185af1f3cf2294f7aed5725534c7b94
unknown <diego.resendez@yahoo.com>
2020-05-02 10:11:13 -0700

    Saturday morning pull

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 
 let cocktailURL = "https://rapidapi.com/theapiguy/api/the-cocktail-db?endpoint=apiendpoint_67a71546-a41f-4dd0-9c04-f54fa8f375ef"
 
-    var searchtheapiguy = function(cocktail) {
-      var queryURL = "";
+    let searchtheapiguy = function(cocktail) {
+      let queryURL = "https://the-cocktail-db.p.rapidapi.com/list.php";
       $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
         createRow(response);
-      });
+      }); console.log(response)
     };
+
